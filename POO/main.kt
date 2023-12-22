52418e71ae4423e6707b1b52f02442289af4f025
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2023-12-22 22:24:16 +0000

    POO - Enumeradores

--- POO/main.kt
+++ POO/main.kt
@@ -30,6 +30,42 @@ class Animal(val especie: String) {
 
 
 
+class Maquina () {
+  var estado: Boolean? = null
+    get () {
+      println("Obtendo valor do estado da máquina...")
+      return field
+    }
+    set (valor) {
+      println("Setando valor do estado da máquina...")
+      field = valor
+    }
+}
+
+
+
+enum class notasMedias (val nota: Int) {
+  baixa(5) {
+    override fun toString(): String {
+      return "Prioridade alta por média ${nota}"
+    }
+  },
+  
+  media(7) {
+    override fun toString(): String {
+      return "Prioridade intermediária por média ${nota}"
+    }
+  },
+  
+  alta(9) {
+    override fun toString(): String {
+      return "Prioridade alta por média ${nota}"
+    }
+  }
+}
+
+
+
 fun main() {
   /*
   // CRIANDO PRIMEIRA CLASSE
@@ -56,8 +92,26 @@ fun main() {
 
 
 
+  /*
   // INIT
   val gato: Animal = Animal("gato")
   gato.fala()
+  */
   
+
+
+
+
+  /*
+  val cortadorDeGrama: Maquina = Maquina()
+  cortadorDeGrama.estado
+  cortadorDeGrama.estado = true
+  */
+
+
+
+
+  for(indice in notasMedias.values()) {
+    println("Índice do Enum: ${indice}; Valor do Enum: ${indice.nota}")
+  }
 }
