cdccc6e19b392f82eaf64f2c2f4905aa37b77a0b
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2023-12-26 15:46:31 +0000

    Java - Controles de Fluxo - Precedência entre Operadores Matemáticos

---
+++ java_controle_e_fluxo/precedencia_operadores_matematicos.java
@@ -0,0 +1,16 @@
+public class precedencia_operadores_matematicos {
+  public static void main(String[] args) {
+
+    /*
+      PRECEDÊNCA DOS OPERADORES SEM PARÊNTESES
+      1° - Divisão (/)
+      2° - Multiplicação (*)
+      3° - Resto da divisão (%)
+      4°- Adição e subtração (+ e -)
+    */
+
+    int x = 10/2*3+4*3;
+    System.out.println(x);
+    
+  }
+}
