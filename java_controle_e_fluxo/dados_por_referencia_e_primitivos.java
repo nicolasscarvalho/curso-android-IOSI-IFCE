89e231c20447f8c9ca21f9221ab27b780cfb1533
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2023-12-26 15:26:28 +0000

    Java - Controles de Fluxo - Dados por referência e tipos de dados primitivos

---
+++ java_controle_e_fluxo/dados_por_referencia_e_primitivos.java
@@ -0,0 +1,25 @@
+class dados_por_referencia_e_primitivos {
+  public static void main(String[] args) {
+    // TIPOS POR REFERÊNCIA -> Objetos com métodos já prontos
+    Byte rByte = 5;
+    Short rShort = 5;
+    Integer rInteger = 5;
+    Long rLong = 5L;
+    Float rFloat = 5.0F;
+    Double rDouble = 5.0;
+    Boolean rBoolean = true;
+    String rString = "Java";
+    Character rCharacter = 'J';
+
+
+    // TIPOS PRIMITIVOS -> Apenas definem o tipo de dado
+    byte pByte = 5;
+    short pShort = 5;
+    int pInteger = 5;
+    long pLong = 5L;
+    float pFloat = 5.0F;
+    double pDouble = 5.0;
+    boolean pBoolean = true;
+    char pCharacter = 'J';
+  }
+}
