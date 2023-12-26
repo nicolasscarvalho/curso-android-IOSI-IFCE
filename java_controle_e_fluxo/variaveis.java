1bc3dc712db6b9eb9fcff63d2884583ad931c289
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2023-12-26 15:25:44 +0000

    Java - Controles de Fluxo - Variáveis e suas propriedades

---
+++ java_controle_e_fluxo/variaveis.java
@@ -0,0 +1,29 @@
+public class variaveis {
+  public static void main(String[] args) {
+    String nome = "Nícolas";
+    System.out.println("Mínimo do Double: " + Double.MIN_VALUE + " Máximo do Double: " + Double.MAX_VALUE);
+    System.out.println("Mínimo do Float: " + Float.MIN_VALUE + " Máximo do Float: " + Float.MAX_VALUE);
+    System.out.println("Mínimo do Long: " + Long.MIN_VALUE + " Máximo do Long: " + Long.MAX_VALUE);
+    System.out.println("Mínimo do Integer: " + Short.MIN_VALUE + " Máximo do Short: " + Short.MAX_VALUE);
+    System.out.println("Mínimo do Short: " + Short.MIN_VALUE + " Máximo do Short: " + Short.MAX_VALUE);
+    System.out.println("Mínimo do Byte: " + Byte.MIN_VALUE + " Máximo do Byte: " + Byte.MAX_VALUE);
+  }
+}
+
+/*
+  TIPOS DE DADOS
+
+  Tipo      Bit
+  -------------
+  Double    64
+  Float     32
+
+  Long      64
+  Integer   32
+  Short     16
+  Byte       8
+
+  Boolean    ?
+  String     ?
+  Character  ?
+*/
