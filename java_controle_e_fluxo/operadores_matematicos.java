1bc5a01222cdc31781fc5a09da5a9fd800799288
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2023-12-26 15:27:14 +0000

    Java - Controles de Fluxo - Operadores Matemáticos

---
+++ java_controle_e_fluxo/operadores_matematicos.java
@@ -0,0 +1,18 @@
+public class operadores_matematicos {
+  public static void main(String[] args) {
+
+    System.out.println("10 + 3 = " + (10+3));
+    System.out.println("10 - 3 = " + (10-3));
+    System.out.println("10 * 3 = " + (10*3));
+    System.out.println("10 / 3 = " + (10/3));
+    System.out.println("10 % 3 = " + (10%3));
+
+    int x = 10;
+    System.out.println("x = " + x);
+    x += 3;
+    System.out.println("x = " + x);
+
+    System.out.println("10.0 / 3.0 = " + (10.0/3.0));
+    
+  }
+}
