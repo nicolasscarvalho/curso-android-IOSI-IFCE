7850ec0c13ae84ae0063ffa4f465a95b2587e62d
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-09 14:39:00 +0000

    Java - Controles de Fluxo - Funções matemáticas

---
+++ java_controle_e_fluxo/funcoes_matematicas.java
@@ -0,0 +1,13 @@
+public class FuncoesMatematicas {
+    public static void main(String[] args) {
+      System.out.println("Máximo do conjunto (10, 20): " + Math.max(10, 20));
+      System.out.println("Mínimo do conjunto (10, 20): " + Math.min(10,20));
+      System.out.println("Raiz quadrada de 100: " + Math.sqrt(100.0));
+      System.out.println("log de 100 na base 10: " + Math.log10(100));
+      System.out.println("2^3: " + Math.pow(2, 3));
+      System.out.println("constante Pi: " + Math.PI);
+      System.out.println("Constante E: " + Math.E);
+      System.out.println("Arredondando 2.3399: " + Math.round(2.3399));
+      System.out.println("Número aleatório gerado: " + Math.random());
+    }
+}
