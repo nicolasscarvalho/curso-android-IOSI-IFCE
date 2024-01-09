6c10254c901267210628f319050ffd190ed012ce
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-09 23:38:23 +0000

    Java - Controles de Fluxo - Operador ternário

---
+++ java_controle_e_fluxo/operador_ternario.java
@@ -0,0 +1,7 @@
+public class OperadorTernario {
+  public static void main(String[] args) {
+    int idade = 18;
+    String mensagem = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+    System.out.println(mensagem);
+  }
+}
