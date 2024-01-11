8658220d60718dc4e8422422b9f58b327e1a5f6a
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-11 14:36:02 +0000

    Java - Controles de Fluxo - Estrutura while

---
+++ java_controle_e_fluxo/estrutura_while.java
@@ -0,0 +1,11 @@
+public class EstruturaWhile {
+  public static void main(String[] args) {
+    int i = 0;
+    
+    while (i < 12) {
+      System.out.println("O valor de i é: " + i);
+      i+=2;
+    }
+    
+  }
+}
