20742a764aadc7c8e6ceb24f97106737d86a3563
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-11 15:10:35 +0000

    Java - Controles de Fluxo - Enhanced for

---
+++ java_controle_e_fluxo/enhanced_for.java
@@ -0,0 +1,11 @@
+public class EnhancedFor {
+  public static void main(String[] args) {
+
+    int[] idades = {10, 20, 30, 40, 50};
+
+    for (int idade : idades) {
+      System.out.println(idade);
+    }
+    
+  }
+}
