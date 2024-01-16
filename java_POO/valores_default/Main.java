67b0f30e4fa71a77a605b01d31fc564b7f0da6fd
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-16 23:09:55 +0000

    Java - POO - Valores default

---
+++ java_POO/valores_default/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args){
+    Valores valores = new Valores();
+    System.out.println(valores.i);
+    System.out.println(valores.j);
+    System.out.println(valores.k);
+    System.out.println(valores.l);
+    System.out.println(valores.m);
+  }
+}
---
+++ java_POO/valores_default/Valores.java
@@ -0,0 +1,9 @@
+public class Valores {
+
+    int i;
+    float j;
+    double k;
+    char l;
+    boolean m;
+    
+}
