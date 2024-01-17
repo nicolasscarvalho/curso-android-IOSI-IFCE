0b64292441603edf6417b686273e6ce7753f7bfb
nicolasscarvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-01-17 14:05:57 +0000

    Adicionando explicação do código (Static)

--- java_POO/atributos_e_metodos_estaticos/Estatico.java
+++ java_POO/atributos_e_metodos_estaticos/Estatico.java
@@ -1,7 +1,7 @@
 public class Estatico {
-  public static int x = 10;
+  public static int x = 10; // Métodos e atributos estáticos não necessitam instanciar um objeto para serem acessados.
 
-  public static void update_x (int new_x) {
+  public static void update_x (int new_x) { // Métodos estáticos podem acessar e modificar apenas atributos estáticos 
     x = new_x;
   }
   
